31b1fc70485a3cedc9922e354ed0c225dd7870d1
Sheikh-Sobuj <sheikh.sobuj95@gmail.com>
2024-09-27 11:33:01 +0600

    focus-education

--- src/app/module/focus/focus.module.ts
+++ src/app/module/focus/focus.module.ts
@@ -11,6 +11,19 @@ import {FocusNutritionComp} from "./focus-nutrition/FocusNutritionComp";
 import {FocusWorldPeaceComp} from "./focus-world-peace/FocusWorldPeaceComp";
 import {FocusYouthComp} from "./focus-youth/FocusYouthComp";
 import { EducationForWomenComp } from './focus-education/education-for-women/EducationForWomenComp';
+import {BloodDonationCampComp} from "./focus-education/blood-donation-camp/BloodDonationCampComp";
+import {DigitalSchoolComp} from "./focus-education/digital-school/DigitalSchoolComp";
+import {DistanceLearningComp} from "./focus-education/distance-learning/DistanceLearningComp";
+import {
+  EducationalForAllSectionDetailsComp
+} from "./focus-education/education-for-all-section-details/EducationalForAllSectionDetailsComp";
+import {EducationForChildrenComp} from "./focus-education/education-for-children/EducationForChildrenComp";
+import {
+  EducationalForChildrenSectionDetailsComp
+} from "./focus-education/education-for-children-section-derails/EducationalForChildrenSectionDetailsComp";
+import {EducationNutritionComp} from "./focus-education/focus-nutrition/EducationNutritionComp";
+import {ReadTogetherComp} from "./focus-education/read-together/ReadTogetherComp";
+import {ScholarshipComp} from "./focus-education/scholarship/ScholarshipComp";
 @NgModule({
   declarations: [
     FocusComponent,
@@ -23,7 +36,15 @@ import { EducationForWomenComp } from './focus-education/education-for-women/Edu
     FocusWorldPeaceComp,
     FocusYouthComp,
     EducationForWomenComp,
-
+    BloodDonationCampComp,
+    DigitalSchoolComp,
+    DistanceLearningComp,
+    EducationalForAllSectionDetailsComp,
+    EducationForChildrenComp,
+    EducationalForChildrenSectionDetailsComp,
+    EducationNutritionComp,
+    ReadTogetherComp,
+    ScholarshipComp,
     ],
 
   imports: [
